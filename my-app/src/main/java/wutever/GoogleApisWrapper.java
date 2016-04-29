ab9a3fb5a9ffc3cea26fe25795ae0131797c7047
Selah Lynch <selahlynch@gmail.com>
2016-04-29 13:34:22 -0400

    Twitter code is cleaned up and contained

---
+++ my-app/src/main/java/wutever/GoogleApisWrapper.java
@@ -0,0 +1,62 @@
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import twitter4j.JSONException;
+import twitter4j.JSONObject;
+
+
+public class GoogleApisWrapper {
+
+	//do fuck all for starters... but changes are it will come in useful eventually
+	GoogleApisWrapper(){}
+	
+	public static Double[] getLatLongForZip(String zipcode){
+		Double[] latLong = new Double[2];
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		String urlString = "http://maps.googleapis.com/maps/api/geocode/json?address="+zipcode;
+		System.out.println("Requesting " + urlString);
+		HttpGet httpGet = new HttpGet(urlString);
+		CloseableHttpResponse response1 = null;
+    try {
+	    response1 = httpclient.execute(httpGet);
+	    System.out.println(response1.getStatusLine());
+	    HttpEntity entity1 = response1.getEntity();
+      if (entity1 != null) {
+        String retSrc = EntityUtils.toString(entity1); 
+        // parsing JSON
+        JSONObject result = new JSONObject(retSrc).getJSONArray("results").getJSONObject(0); //Convert String to JSON Object
+        String latStr = result.getJSONObject("geometry").getJSONObject("location").getString("lat");
+        String lngStr = result.getJSONObject("geometry").getJSONObject("location").getString("lng");
+        latLong[0] = Double.parseDouble(latStr);
+        latLong[1] = Double.parseDouble(lngStr);
+      }    		    // do something useful with the response body
+	    EntityUtils.consume(entity1); //Must fully consume to avoid resource leak
+    } catch (ClientProtocolException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+    } catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+    } catch (JSONException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+    } finally {
+		    try {
+	        response1.close();
+        } catch (IOException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+        } //MUST close to avoid resource leak
+		}
+		return latLong;
+	}
+	
+	
+}
--- my-app/src/main/java/wutever/Main.java
+++ my-app/src/main/java/wutever/Main.java
@@ -3,13 +3,9 @@ import static spark.Spark.*;
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
 import twitter4j.GeoLocation;
-import twitter4j.Location;
 import twitter4j.Query;
 import twitter4j.QueryResult;
-import twitter4j.ResponseList;
 import twitter4j.Status;
-import twitter4j.Trend;
-import twitter4j.Trends;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -25,42 +21,39 @@ public class Main {
     	
       ACSData myACSData = new ACSData();
       HashMap<String,HashMap<String, String>> ACSbyZIP = myACSData.getACSData();
-
-      //useful zipcode to center coordinates map
-      // http://maps.googleapis.com/maps/api/geocode/json?address=19143
-
-      Twitter twitter = TwitterFactory.getSingleton();  
-      GeoLocation coords19104 = new GeoLocation(39.9584, -75.1954);
-      GeoLocation coords19143 = new GeoLocation(39.9413, -75.2187);
-      GeoLocation coords19142 = new GeoLocation(39.9227, -75.2304);
-      Query q = new Query();
-    	q = q.geoCode(coords19142, 0.5, "mi");
-      QueryResult qr = null;
-      try {
-	      qr = twitter.search(q);
-	      System.out.println("IT WORKED IT WORKED!! :)");
-      } catch (TwitterException e) {
-	      System.out.println("Twitter request DID NOT WORK :(");
-	      e.printStackTrace();
-      }
-
-      List<Status> tweets;
-
-      tweets = qr.getTweets();
-      System.out.println("Printing "+tweets.size()+" tweets from 19142...");
-      for(int i =0; i<tweets.size(); i++){
-      	System.out.println(tweets.get(i).getText());
-      }
       
     	get("/TESTZIP/:zipcode", (request, response) -> new ModelAndView(ACSbyZIP.get(request.params(":zipcode")), "test.mustache"), new MustacheTemplateEngine());
 
-    	Map<String, Object> viewObjects = new HashMap<String, Object>();
-    	viewObjects.put("atrend", tweets.get(0).getText());
     	
-    	get("/TESTTWITTER/:ZIP", (request, response) -> new ModelAndView(viewObjects, "twitter.mustache"), new MustacheTemplateEngine());
+    	get("/TESTTWITTER/:ZIP", (request, response) -> {
+    		//initiate values for use with mustache template
+    		Map<String, Object> viewObjects = new HashMap<String, Object>();    		
 
-    	get("/hello/:ZIP", (request, response) -> {
-        String msg =  "Hello: " + request.params(":name");
+      	//get latitude and longitude from GoogleAPI
+      	Double[] latLong = GoogleApisWrapper.getLatLongForZip(request.params(":ZIP"));
+      	viewObjects.put("lat", latLong[0]);    		
+      	viewObjects.put("lng", latLong[1]);    		
+
+      	//get tweets from twitter API
+      	GeoLocation coords = new GeoLocation(latLong[0], latLong[1]);
+      	Twitter twitter = TwitterFactory.getSingleton();  
+        Query q = new Query();
+      	q = q.geoCode(coords, 0.5, "mi");
+        QueryResult qr = null;
+        try {
+  	      qr = twitter.search(q);
+        } catch (TwitterException e) {
+  	      System.out.println("Twitter request DID NOT WORK :(");
+  	      e.printStackTrace();
+        }
+        List<Status> tweets;
+        tweets = qr.getTweets();
+        String tweetsString = "Tweets:";
+        for(int i =0; i<tweets.size(); i++){
+        	tweetsString += tweets.get(i).getText();
+        }
+      	viewObjects.put("alltweets", tweetsString);    		
+    		
         ModelAndView mv = new ModelAndView(viewObjects, "twitter.mustache");
         MustacheTemplateEngine mte = new MustacheTemplateEngine();
         return mte.render(mv);
