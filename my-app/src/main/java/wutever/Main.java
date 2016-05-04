f4c5db93081d5a51d1f76a1e3c8c50ec036d2688
Britt Binler <brittany.binler@gmail.com>
2016-05-03 23:38:27 -0400

    updated heroku dependencies

--- my-app/src/main/java/wutever/Main.java
+++ my-app/src/main/java/wutever/Main.java
@@ -25,7 +25,7 @@ import org.json.JSONException;
 public class Main {
 	
     public static void main(String[] args) throws JSONException, IOException {
-    	
+    	port(getHerokuAssignedPort());
     	staticFileLocation("/resources/templates");
     	Spark.staticFileLocation("/public");
     	
@@ -244,4 +244,12 @@ public class Main {
     	return retVal;
     }
 	
+    
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
